1082feb93cb6f1b6f1b0cfcd8cce0c58a2d9f220
Sergei <persona.four@yandex.ru>
2024-02-09 15:08:06 +0300

    add task 3

--- 3/Program.cs
+++ 3/Program.cs
@@ -1,1 +1,10 @@
-﻿
+﻿int[] arr = {1, 2, 5, 0, 10, 34}; 
+        void ShowReversedArray (int[] array, int i = 0)
+        {
+            if (i != array.Length)
+            {
+            ShowReversedArray(array, i+1);
+            Console.Write(array[i] + " ");
+            }
+        }
+        ShowReversedArray (arr);
